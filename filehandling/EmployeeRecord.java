b746e8a070a8852dc3498d765fdb8e9be29fa0de
mdshahnawaj <mdshahnawaj.cs21@gmail.com>
2023-07-03 11:54:43 +0530

    Commit practical v2

--- filehandling/EmployeeRecord.java
+++ filehandling/EmployeeRecord.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.io.FileWriter;
 
 /**
- * @author MdShahnawaj
  * This class is used to store data in file
+ * @author MdShahnawaj
 */
 class EmployeeRecord {
 
@@ -13,10 +13,10 @@ class EmployeeRecord {
 		File file = new File("D:\\FileHandling\\Student.txt");
 		try {
 			if (file.createNewFile()) {
-				//This is file writer class
+				/** This is file writer class */
 				FileWriter fileWriter = new FileWriter(file);
 
-				//Write data in file using write function
+				/** Write data in file using write function */
 				fileWriter.write("Name : Shahnawaj \nCompany : QDev Technolab \nEmail : msd123@gmail.com");
 				System.out.println("Information successfully added :");
 				fileWriter.close();
--- filehandling/Read.java
+++ filehandling/Read.java
@@ -3,19 +3,19 @@ package filehandling;
 import java.io.FileReader;
 
 /**
- * @author MdShahnawaj
  * Class is used to read data from file
+ * @author MdShahnawaj
 */
 public class Read {
 
 	public static void main(String[] args) {
 		try {
-			//This is FileReader class
+			/** This is FileReader class */
 			FileReader fileReader = new FileReader("D:\\FileHandling\\Student.txt");
 			int data;
 			System.out.println("Employee details :");
 
-			//This loop is used to fetch data in a file
+			/** This loop is used to fetch data in a file */
 			while ((data = fileReader.read()) != -1) {
 				System.out.print((char) data);
 			}
