e7b8d7b0536799bcda2bb7b38adde82b7b3fd64a
mdshahnawaj <136331270+mdshahnawaj@users.noreply.github.com>
2023-07-04 19:19:28 +0530

    Commit practical v2

--- BasicProgramme/StringMethod.java
+++ BasicProgramme/StringMethod.java
@@ -7,8 +7,8 @@ package BasicProgramme;
 public class StringMethod {
 
 	public static void main(String[] args) {
-		String nameOne = "stringone";
-		String nameTwo = "stringtwo";
+		String nameOne = "stringOne";
+		String nameTwo = "stringTwo";
 
 		/** String method to perform string operation */
 		System.out.println("Find index position in string : " + nameOne.charAt(3));
